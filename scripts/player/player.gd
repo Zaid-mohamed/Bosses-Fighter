5a307a777554fefb3fc8a4c95aeb3e778cf32a10
Zaid-mohamed <zaidmohamedhmd@gmail.com>
2024-06-04 00:25:46 +0300

    Made player look where he goes and (read description)
    edietd the way that the idle animation work

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -10,9 +10,20 @@ extends CharacterBody2D
 @export var accel : float
 # deceleration
 @export var decel : float
+
+
+# onready vars
+
+@onready var body: Sprite2D = %body
+@onready var anim: AnimationPlayer = %Anim
+
+
+
+
 func _physics_process(delta: float) -> void:
 	movement()
-
+	
+	handle_facing()
 
 
 
@@ -29,3 +40,10 @@ func movement():
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, decel * get_physics_process_delta_time())
 	move_and_slide()
+
+# handles the direction the player faces
+func handle_facing():
+	if Input.is_action_just_pressed("ui_left"):
+		body.flip_h = true
+	elif Input.is_action_just_pressed("ui_right"):
+		body.flip_h = false
