3a7770f245a4e467013e9631735ff699178ce5b9
Zaid-mohamed <zaidmohamedhmd@gmail.com>
2024-06-10 20:16:40 +0300

    Mircro Commit

--- scripts/player/Inventory.gd
+++ scripts/player/Inventory.gd
@@ -17,13 +17,15 @@ func _ready() -> void:
 	# connect the inventory interacted signal
 	# from the inventory_data resource of the player inventory dialog.
 	player_inventory_dialog.inventory_data.inventory_interacted.connect(_inventory_interacted)
+	# connect the inventory interacted signal
+	# from the inventory_data resource of the player hotbar dialog.
 	hotbar.inventory_data.inventory_interacted.connect(_inventory_interacted)
 	
 
 
 func _process(delta: float) -> void:
 	# make the grabbed_slot follow the mouse with lerp
-	grabbed_slot.global_position = lerp(grabbed_slot.global_position, get_child(0).get_global_mouse_position() + Vector2.ONE * 5, 0.5)
+	grabbed_slot.global_position = lerp(grabbed_slot.global_position, get_child(0).get_global_mouse_position(), 0.5)
 	
 	
 	# open and close inventory_dialog.
@@ -43,7 +45,7 @@ func _inventory_interacted(inventory_data : InventoryData , button_index, slot_i
 			grabbed_slot_data = inventory_data.grab_slot_data(slot_index)
 			# set the slot_data of the grabbed_slot to the grabbed_slot_data.
 			grabbed_slot.set_slot_data(grabbed_slot_data) 
-			# update the inventory (to make the effect visually).
+			# update the inventory or hotbar (to make the effect visually).
 			dialog.update_inventory_dialog(inventory_data)
 			# show the grabbed_slot if the grabbed_slot_data got a value from inventroy.grab_slot_data function above
 			if grabbed_slot_data : grabbed_slot.show()
@@ -52,12 +54,12 @@ func _inventory_interacted(inventory_data : InventoryData , button_index, slot_i
 		[_, MOUSE_BUTTON_LEFT]:
 			#check if we can not put the data we have to the a slot with the given index.
 			if !inventory_data.put_slot_data(slot_index, grabbed_slot.slot_data):
-				# if so update the inventory and return (dont continue).
+				# if so update the inventory or hotbar and return (dont continue).
 				dialog.update_inventory_dialog(inventory_data)		
 				return
 			# if yes :
 			
-			# update the inventory.
+			# update the inventory or hotbar.
 			dialog.update_inventory_dialog(inventory_data)
 			# make the grabbed_slot_data null (because the put this data in a slot in the inventory).
 			grabbed_slot_data = null
