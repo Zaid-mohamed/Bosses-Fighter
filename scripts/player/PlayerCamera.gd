2b2787ff10841d81178c7c3f6264f352aeb47978
Zaid-mohamed <zaidmohamedhmd@gmail.com>
2024-06-12 16:26:58 +0300

    EnhancedCameraOffset
    according to mouse position and player veocity

---
+++ scripts/player/PlayerCamera.gd
@@ -0,0 +1,26 @@
+extends Camera2D
+class_name PlayerCamera
+
+
+@export var player_node : CharacterBody2D
+
+
+@export_group("Offsets")
+
+## the offset length that the camera when camera offset towards the mouse position
+@export var mouse_offset : float = 10.0
+
+## same as above but offset according to player velocity
+@export var velocity_offset : float = 10.0
+
+
+func _process(delta: float) -> void:
+	if player_node.velocity == Vector2.ZERO:
+		change_offset_according_to_mouse_position() 
+	else:
+		change_offset_according_to_player_velocity()
+		
+func change_offset_according_to_player_velocity():
+	offset = offset.lerp(player_node.velocity.normalized() * velocity_offset, 0.07)
+func change_offset_according_to_mouse_position():
+	offset = offset.lerp((get_global_mouse_position() - player_node.global_position).normalized() * mouse_offset, 0.07)
