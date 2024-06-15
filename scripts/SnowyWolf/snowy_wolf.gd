53320d094cf9b1be7a3e467eed186869d50f0a4b
devlamingo <168713883+devlamingo@users.noreply.github.com>
2024-06-16 01:10:16 +0300

    FinishSomeFunction
    Finished Idle State, FellDown, IdleMind, Center Location

--- scripts/SnowyWolf/snowy_wolf.gd
+++ scripts/SnowyWolf/snowy_wolf.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 class_name SnowyWolf
 
 # Constants Of State Machine
-enum States{Idle, RunToPlayer, ThrowSnowBall, CopyOfShadow, SnowBallStorm, Die}
+enum States{Idle, RunToPlayer, ThrowSnowBall, CopyOfShadow, SnowBallStorm, FellDown, Die}
 
 # Boss's Phases
 enum Phases{Easy, Normal, Angry}
@@ -19,20 +19,31 @@ const max_health = 350
 
 var health = max_health
 
+# Center Location Of The Arena.
+var center_location = Vector2(812, 606)
+
+# Player Node
+@onready var player_node = $"../player"
+
+
 # Process Function
 func _process(delta: float):
 	pass
 
 # Change Phase to Harder One
-func select_phase(next_phase: Phases):
-	pass
-
+#func select_phase(next_phase: Phases):
+#pass
+#
 # Change State to The Next State
-func change_state(next_state: States):
-	pass
+#func change_state(next_state: States):
+#pass
 
 # Idle State
 func idle_state():
+	velocity = Vector2.ZERO
+	if position != center_location:
+		position = center_location
+	$IdleMind.start()
 	pass
 
 # RunToPlayer State
@@ -51,14 +62,38 @@ func copy_of_shadow_state():
 func snowball_storm_state():
 	pass
 
+# Fell Down State
+func fell_down_state():
+	pass
+
 # Die State
 func die_state():
 	pass
 
 # Idle Mind Timeout Function/Signal.
 func _on_idle_mind_timeout():
+	match current_phase:
+		Phases.Easy:
+			var RandomizeEasy = randi_range(0, 1)
+			match RandomizeEasy:
+				0: current_state = States.RunToPlayer
+				1: current_state = States.ThrowSnowBall
+		Phases.Normal:
+			var RandomizeNormal = randi_range(0, 4)
+			match RandomizeNormal:
+				0: current_state = States.RunToPlayer
+				1: current_state = States.ThrowSnowBall
+				_: current_state = States.CopyOfShadow
+		Phases.Angry:
+			var RandomizeAngry = randi_range(0, 8)
+			match RandomizeAngry:
+				0: current_state = States.RunToPlayer
+				1: current_state = States.ThrowSnowBall
+				2 || 3: current_state = States.CopyOfShadow
+				_: current_state = States.SnowBallStorm
 	pass
 
 # Fell Down Timeout Function/Signal.
 func _on_fell_down_timeout():
+	current_state = States.Idle
 	pass
