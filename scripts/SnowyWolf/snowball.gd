c066e61f9c963e601e7b631db493c4134b5bd711
devlamingo <168713883+devlamingo@users.noreply.github.com>
2024-06-19 22:38:22 +0300

    simple_changes_on_boss
    signal mechanic - some edits, added scorpion boss aseprite file - png

--- scripts/SnowyWolf/snowball.gd
+++ scripts/SnowyWolf/snowball.gd
@@ -4,6 +4,3 @@ class_name Snowball
 
 func _physics_process(delta):
 	move_and_slide()
-
-func _on_hitbox_body_entered(body):
-	pass
--- scripts/SnowyWolf/snowy_wolf.gd
+++ scripts/SnowyWolf/snowy_wolf.gd
@@ -2,16 +2,29 @@ extends CharacterBody2D
 
 class_name SnowyWolf
 
+# Enum(s)
 enum States{ Idle, RunToPlayer, ThrowSnowBall, CopyOfShadow, SnowBallStorm, FellDown, Die }
 enum Phases{ Easy, Normal, Angry }
-var current_state = States.ThrowSnowBall
-var current_phase = Phases.Easy
-var center_location = Vector2(812, 606)
-var speed = 120
+var current_state : States = States.Idle
+var current_phase : Phases = Phases.Easy
+
+
+var center_location : Vector2 = Vector2(810, 634)
+var run_speed : int = 160
+var player_direction = null
 @onready var player_node = $"../player"
 
-func _process(delta: float):
-	match current_state:
+signal state_changed(new_state : States)
+
+func _ready():
+	state_changed.connect(_on_snowywolf_state_changed)
+	emit_signal("state_changed", States.Idle)
+	
+func _on_snowywolf_state_changed(new_state : States):
+	
+	## Manage state machine states.
+	
+	match new_state:
 		States.Idle:
 			idle_state()
 		States.RunToPlayer:
@@ -26,33 +39,61 @@ func _process(delta: float):
 			fell_down_state()
 		States.Die:
 			die_state()
-	
 
 func idle_state():
+	# Boss can't move:
 	velocity = Vector2.ZERO
+	
+	# Put the boss the center of the arena.
 	if position != center_location:
 		position = center_location
-	$IdleMind.start()
+	
+	# State the Changer decide timer. 
+	$IdleMind.start(2.5)
+	
+	# Reset the player direction.
+	player_direction = null
 
 func run_to_player_state():
-	var direction = (player_node.global_position - global_position).normalized()
-	velocity = direction * speed
+	## Decide the direction to move to, one line.
+	if player_direction == null:
+		player_direction = (player_node.global_position - global_position).normalized()
+	
+	velocity = player_direction * run_speed
+	
 	move_and_slide()
-
+	
+	## if Collide with a wall, will fell down, then the player can damage it.
+	##
+	## DON'T TOUCH IT NOW, WHEN I FINISHED
+	current_state = States.Idle # Fell Down
+	emit_signal("state_changed", current_state)
+	
 func throw_snowball_state():
+	# Boss can't move:
 	velocity = Vector2.ZERO
+	
+	# Put the boss the center of the arena.
 	if position != center_location:
 		position = center_location
-	var player_direction = (player_node.global_position - global_position).normalized()
+	
+	# Decide the player direction to throw the snowball.
+	player_direction = (player_node.global_position - global_position).normalized()
+	
+	# Snowball Instantiate Configure
 	var snowball_instance = preload("res://scenes/SnowyWolf/snowball.tscn").instantiate()
 	get_parent().add_child(snowball_instance)
+	
 	snowball_instance.global_position = global_position
+	
 	var snowball_speed = 200 
 	snowball_instance.velocity = player_direction * snowball_speed
+	
+	# Change the state to The IDLE again.
 	current_state = States.Idle
+	emit_signal("state_changed", current_state)
 
 func copy_of_shadow_state():
-	# Example: Implement shadow copying logic
 	pass
 
 func snowball_storm_state():
@@ -71,7 +112,9 @@ func _on_idle_mind_timeout():
 	match current_phase:
 		Phases.Easy:
 			var RandomizeEasy = randi_range(0, 1)
-			current_state = (RandomizeEasy == 0) if States.RunToPlayer else States.ThrowSnowBall
+			match RandomizeEasy:
+				0: current_state = States.RunToPlayer
+				1: current_state = States.ThrowSnowBall
 		Phases.Normal:
 			var RandomizeNormal = randi_range(0, 4)
 			match RandomizeNormal:
@@ -85,6 +128,8 @@ func _on_idle_mind_timeout():
 				1: current_state = States.ThrowSnowBall
 				2, 3: current_state = States.CopyOfShadow
 				_: current_state = States.SnowBallStorm
+	emit_signal("state_changed", current_state)
 
 func _on_fell_down_timeout():
 	current_state = States.Idle
+	emit_signal("state_changed", current_state)
