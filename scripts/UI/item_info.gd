b7a0de4c1e28c955d5158899e99e483edd91088d
Zaid-mohamed <zaidmohamedhmd@gmail.com>
2024-06-21 21:37:27 +0300

    added comments to item_info.gd and edited the colors of the item_info scene

--- scripts/UI/item_info.gd
+++ scripts/UI/item_info.gd
@@ -1,23 +1,29 @@
 extends PanelContainer
 class_name ItemInfo
 
-
+# the item that the UI shows the info about
 @export var item_data : ItemData
 
+
+# the nodes :
+
+# the first texture that shows the texture of the item
 @onready var item_preview: TextureRect = %ItemPreview
+# the label that shows the title (the item name)
 @onready var title: Label = %Title
+# the second texture that shows the texture of the item
 @onready var item_preview_2: TextureRect = %ItemPreview2
+# the label that shows the description of the item
 @onready var description: Label = %Description
+# the ok button that closes the window
 @onready var ok_button: Button = %OkButton
 
+# the default scale of the window (useful in tweens)
 var default_scale : Vector2
+
 func _ready() -> void:
+	# give the default scale the  value of the scale 
 	default_scale = scale
-	if !item_data: return
-	item_preview.texture = item_data.texture
-	item_preview_2.texture = item_data.texture
-	title.text = item_data.Name
-	description.text = item_data.Description
 
 
 
