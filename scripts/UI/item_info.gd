e298f3b3fbf0ce44278b39f132a9e8b9468a7056
Zaid-mohamed <zaidmohamedhmd@gmail.com>
2024-06-21 21:29:35 +0300

    Improved Item Info

--- scripts/UI/item_info.gd
+++ scripts/UI/item_info.gd
@@ -10,8 +10,9 @@ class_name ItemInfo
 @onready var description: Label = %Description
 @onready var ok_button: Button = %OkButton
 
-
+var default_scale : Vector2
 func _ready() -> void:
+	default_scale = scale
 	if !item_data: return
 	item_preview.texture = item_data.texture
 	item_preview_2.texture = item_data.texture
@@ -22,7 +23,7 @@ func _ready() -> void:
 
 func close():
 	var close_tween = create_tween()
-	close_tween.tween_property(self, "scale", Vector2.ONE * 1.5, 0.2)
+	close_tween.tween_property(self, "scale", default_scale * 1.5, 0.2)
 	close_tween.tween_property(self, "scale", Vector2.ZERO, 0.2)
 	await close_tween.finished
 	hide()
@@ -31,8 +32,8 @@ func open():
 	scale = Vector2.ZERO
 	show()
 	var open_tween = create_tween()
-	open_tween.tween_property(self, "scale", Vector2.ONE * 1.2, 0.2)
-	open_tween.tween_property(self, "scale", Vector2.ONE, 0.1)
+	open_tween.tween_property(self, "scale", default_scale * 1.2, 0.2)
+	open_tween.tween_property(self, "scale", default_scale, 0.1)
 func set_item_data(value : ItemData) -> void:
 	if !value : return
 	item_data = value
