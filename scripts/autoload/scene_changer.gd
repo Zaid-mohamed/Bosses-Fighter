83d640f03cc0587fc68bd764c2d8f76e97a02465
Zaid-mohamed <zaidmohamedhmd@gmail.com>
2024-07-01 19:09:54 +0300

    Fixed Scene Transition

--- scripts/autoload/scene_changer.gd
+++ scripts/autoload/scene_changer.gd
@@ -16,31 +16,11 @@ func _ready() -> void:
 # changes the scene with transition
 func change_scene(new_scene_path):
 	# start the close tween and wait until it finished
-	await close().finished
+	anim.play("Close")
+	await anim.animation_finished
 	# then change the scene
 	get_tree().change_scene_to_file(new_scene_path)
 	# move the transition_shape_away
-	open()
+	anim.play("Open")
 
 
-
-# the close tween, it will move the trasition_shape to hide the screen
-func close() -> Tween:
-	
-	var close_tween = create_tween()
-	
-	# move the transition_shape to hide the screen
-	close_tween.tween_property(transition_shape, "position:x", 0.0, 0.5)
-	# reset the position of the transition shape
-	anim.play("RESET")
-	# it returns the tween
-	return close_tween
-
-
-# the open tween, it will move the trasition_shape to show the screen
-func open() -> Tween:
-	var open_tween = create_tween()
-	# move the tranisiton shape away from the screen (to the right)
-	open_tween.tween_property(transition_shape, "position:x", transition_shape.size.x, 0.5)
-	# it returns the tween
-	return open_tween
