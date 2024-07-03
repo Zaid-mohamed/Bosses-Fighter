b1b0547166689d9c51a7dc4b827ae854df4f3e07
Zaid-mohamed <zaidmohamedhmd@gmail.com>
2024-07-03 21:02:57 +0300

    Improved Pause Menu

--- scripts/World/SnowyArena.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-@onready var pause_menu: PauseMenu = $Pause_Menu/Pause_menu
-
-func _process(delta):
-	if Input.is_action_pressed("Pause"):
-		get_tree().paused = true
-		pause_menu.show()
